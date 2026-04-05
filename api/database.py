cdfd2d618f970830f293f078435898c87d0f1ebc
Omolaiye Samuel Oluwatobi <somolaiye53@gmail.com>
2026-04-05 17:43:07 +0100

    Update DB config and requirements for Render deployment

--- api/database.py
+++ api/database.py
@@ -9,10 +9,12 @@ load_dotenv()
 # We'll use SQLite for quick MVP storage, but schema easily migrations to PostgreSQL later
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./lendrisk.db")
 
-# connect_args={"check_same_thread": False} is needed only for SQLite
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
